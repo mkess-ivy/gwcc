db599fd7bfdc6926a337f6ab21e484a00d3f5936
Montier Kess <montier.kess@gmail.com>
2017-11-09 02:36:18 -0500

    first three sections for home mobile done

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    // Smooth Scroll
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+          window.location.hash = target;
+        });
+    });
+
+    // Main Overlay Menu Functionality
+    $("#overlay-menu").click(function() {
+        $(".overlay").addClass("overlay-open");
+        $('body').addClass("noScroll");
+    });
+
+	$(".overlay-close").click(function() {
+        $(".overlay").removeClass("overlay-open");
+        $('body').removeClass("noScroll");
+    });
+
+}); // Close of jQuery
